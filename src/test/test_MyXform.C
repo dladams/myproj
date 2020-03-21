b379437c4a4bc3911ebb225c166d8e25fbba5acf
David Adams <dladams@bnl.gov>
2020-03-21 13:22:04 -0500

    Add help to build.

--- src/test/test_MyXform.C
+++ src/test/test_MyXform.C
@@ -1,3 +1,13 @@
 void test_MyXform() {
-  gROOT->ProcessLine(".class MyXform");
+  //gROOT->ProcessLine(".class MyXform");
+  string myname = "test_MyXform.C: ";
+  string className = "MyXform";
+  cout << myname << "Fetching class " << className << endl;
+  TClass tc(className.c_str());
+  if ( tc.GetClassInfo() == nullptr ) {
+    cout << myname << "Info no not found for class " << className << endl;
+    exit(1);
+  }
+  cout << myname << "Done." << endl;
+  exit(0);
 }
